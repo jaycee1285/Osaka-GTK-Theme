2eab130f078b15daf61ad228256df94770355e6e
@korpsvart <47998118+Fausto-Korpsvart@users.noreply.github.com>
2025-08-20 09:56:38 -0600

    Update!
    Small fixes and improvements

--- themes/src/assets/gtk-2.0/make-assets.sh
+++ themes/src/assets/gtk-2.0/make-assets.sh
@@ -6,76 +6,6 @@ for theme in '' '-Blue' '-Green' '-Grey' '-Orange' '-Pink' '-Purple' '-Red' '-Te
             if [[ "$color" == '' ]]; then
                 case "$theme" in
                     '')
-                        theme_color='#268bd3'
-                        ;;
-                    -Blue)
-                        theme_color='#268bd3'
-                        ;;
-                    -Green)
-                        theme_color='#849900'
-                        ;;
-                    -Grey)
-                        theme_color='#5e6d6e'
-                        ;;
-                    -Orange)
-                        theme_color='#c94c16'
-                        ;;
-                    -Pink)
-                        theme_color='#d23681'
-                        ;;
-                    -Purple)
-                        theme_color='#6d71c4'
-                        ;;
-                    -Red)
-                        theme_color='#db302d'
-                        ;;
-                    -Teal)
-                        theme_color='#29a298'
-                        ;;
-                    -Yellow)
-                        theme_color='#b28500'
-                        ;;
-                esac
-
-                if [[ "$type" == '-Solarized' ]]; then
-                    background_color='#ede7d3'
-
-                    case "$theme" in
-                        '')
-                            theme_color='#268bd3'
-                            ;;
-                        -Blue)
-                            theme_color='#268bd3'
-                            ;;
-                        -Green)
-                            theme_color='#849900'
-                            ;;
-                        -Grey)
-                            theme_color='#5e6d6e'
-                            ;;
-                        -Orange)
-                            theme_color='#c94c16'
-                            ;;
-                        -Pink)
-                            theme_color='#d23681'
-                            ;;
-                        -Purple)
-                            theme_color='#6d71c4'
-                            ;;
-                        -Red)
-                            theme_color='#db302d'
-                            ;;
-                        -Teal)
-                            theme_color='#29a298'
-                            ;;
-                        -Yellow)
-                            theme_color='#b28500'
-                            ;;
-                    esac
-                fi
-            else
-                case "$theme" in
-                    '')
                         theme_color='#1a6397'
                         ;;
                     -Blue)
@@ -85,7 +15,7 @@ for theme in '' '-Blue' '-Green' '-Grey' '-Orange' '-Pink' '-Purple' '-Red' '-Te
                         theme_color='#586600'
                         ;;
                     -Grey)
-                        theme_color='#e1d7b7'
+                        theme_color='#5e6d6e'
                         ;;
                     -Orange)
                         theme_color='#a13c10'
@@ -108,8 +38,7 @@ for theme in '' '-Blue' '-Green' '-Grey' '-Orange' '-Pink' '-Purple' '-Red' '-Te
                 esac
 
                 if [[ "$type" == '-Solarized' ]]; then
-                    background_color='#002b36'
-
+                    background_color='#ede7d3'
                     case "$theme" in
                         '')
                             theme_color='#1a6397'
@@ -121,7 +50,7 @@ for theme in '' '-Blue' '-Green' '-Grey' '-Orange' '-Pink' '-Purple' '-Red' '-Te
                             theme_color='#586600'
                             ;;
                         -Grey)
-                            theme_color='#e1d7b7'
+                            theme_color='#5e6d6e'
                             ;;
                         -Orange)
                             theme_color='#a13c10'
@@ -143,24 +72,90 @@ for theme in '' '-Blue' '-Green' '-Grey' '-Orange' '-Pink' '-Purple' '-Red' '-Te
                             ;;
                     esac
                 fi
+            else
+                case "$theme" in
+                    '')
+                        theme_color='#268bd3'
+                        ;;
+                    -Blue)
+                        theme_color='#268bd3'
+                        ;;
+                    -Green)
+                        theme_color='#849900'
+                        ;;
+                    -Grey)
+                        theme_color='#e1d7b7'
+                        ;;
+                    -Orange)
+                        theme_color='#c94c16'
+                        ;;
+                    -Pink)
+                        theme_color='#d23681'
+                        ;;
+                    -Purple)
+                        theme_color='#6d71c4'
+                        ;;
+                    -Red)
+                        theme_color='#db302d'
+                        ;;
+                    -Teal)
+                        theme_color='#29a298'
+                        ;;
+                    -Yellow)
+                        theme_color='#b28500'
+                        ;;
+                esac
+
+                if [[ "$type" == '-Solarized' ]]; then
+                    background_color='#002b36'
+                    case "$theme" in
+                        '')
+                            theme_color='#268bd3'
+                            ;;
+                        -Blue)
+                            theme_color='#268bd3'
+                            ;;
+                        -Green)
+                            theme_color='#849900'
+                            ;;
+                        -Grey)
+                            theme_color='#e1d7b7'
+                            ;;
+                        -Orange)
+                            theme_color='#c94c16'
+                            ;;
+                        -Pink)
+                            theme_color='#d23681'
+                            ;;
+                        -Purple)
+                            theme_color='#6d71c4'
+                            ;;
+                        -Red)
+                            theme_color='#db302d'
+                            ;;
+                        -Teal)
+                            theme_color='#29a298'
+                            ;;
+                        -Yellow)
+                            theme_color='#b28500'
+                            ;;
+                    esac
+                fi
             fi
 
             if [[ "$type" != '' ]]; then
                 cp -r "assets${color}.svg" "assets${theme}${color}${type}.svg"
-                # cp -r "assets-common${color}.svg" "assets-common${color}${type}.svg"
                 if [[ "$color" == '' ]]; then
-                    sed -i "s/#268bd3/${theme_color}/g" "assets${theme}${color}${type}.svg"
-                    # sed -i "s/#fdf5e2/${background_color}/g" "assets-common${color}${type}.svg"
-                else
                     sed -i "s/#1a6397/${theme_color}/g" "assets${theme}${color}${type}.svg"
-                    # sed -i "s/#001419/${background_color}/g" "assets-common${color}${type}.svg"
+                else
+                    sed -i "s/#268bd3/${theme_color}/g" "assets${theme}${color}${type}.svg"
                 fi
             elif [[ "$theme" != '' ]]; then
                 cp -r "assets${color}.svg" "assets${theme}${color}.svg"
                 if [[ "$color" == '' ]]; then
-                    sed -i "s/#268bd3/${theme_color}/g" "assets${theme}${color}.svg"
-                else
                     sed -i "s/#1a6397/${theme_color}/g" "assets${theme}${color}.svg"
+                else
+                    sed -i "s/#268bd3/${theme_color}/g" "assets${theme}${color}.svg"
                 fi
             fi
 
